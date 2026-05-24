2d9d0763e0d7e7c8b7da09c1ee2e09d2ff056388
Zac Westbrook <zacwestbrook24@gmail.com>
2026-05-23 21:46:07 -0400

    fix alembic env to read DATABASE_URL from environment

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,6 +1,6 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine, engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
@@ -64,11 +64,9 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    import os
+    url = os.getenv("DATABASE_URL", config.get_main_option("sqlalchemy.url"))
+    connectable = create_engine(url, poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
         context.configure(
